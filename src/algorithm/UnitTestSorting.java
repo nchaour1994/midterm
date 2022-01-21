f40d485a38099a6ca950a0869049594dc5a2b824
nabil <nchaour6@gmail.com>
2022-01-21 16:09:37 -0500

    algorithm

--- src/algorithm/UnitTestSorting.java
+++ src/algorithm/UnitTestSorting.java
@@ -1,31 +1,221 @@
 package algorithm;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 public class UnitTestSorting {
 
     /*
       This class is about Unit testing for Sorting Algorithm.
      */
-    public static void main(String[] args) {
-        int [] unSortedArray = {6,9,2,5,1,0,4};
-        int [] sortedArray =   {0,1,2,4,5,6,9};
-        //Create Sort object
-        Sort sort = new Sort();
-        //apply unsorted array to selectionSort.
-        sort.selectionSort(unSortedArray);
-        //verify if the unsorted array is sorted by the selection sort algorithm.
-        try {
-            Assert.assertEquals(sortedArray, unSortedArray);
-        }catch(Exception ex){
-            ex.getMessage();
-        }
+//    public static void main(String[] args) {
+//        int [] unSortedArray = {6,9,2,5,1,0,4};
+//        int [] sortedArray =   {0,1,2,4,5,6,9};
+//        //Create Sort object
+//        Sort sort = new Sort();
+//        //apply unsorted array to selectionSort.
+//        sort.selectionSort(unSortedArray);
+//        //verify if the unsorted array is sorted by the selection sort algorithm.
+//        try {
+//            Assert.assertEquals(sortedArray, unSortedArray);
+//        }catch(Exception ex){
+//            ex.getMessage();
+//        }
 
         //Now implement Unit test for rest of the soring algorithm...................below
+        @Test
+        public void testInsertion(){
+            int [] unSortedArray = {6,9,2,5,1,0,4};
+            int [] sortedArray =   {0,1,2,4,5,6,9};
+            algorithm.Sort s=new algorithm.Sort();
+            int[]res=s.insertionSort(unSortedArray);
+//             for (int i =0 ;i < res.length;i++){
+//                 System.out.println(res[i]);
+//             }
+            Assert.assertArrayEquals(sortedArray,res);
+            System.out.println("test passed ");
 
-
-
-
+        }
+    @Test
+    public void testInsertion1(){
+        int [] unSortedArray = {6,9,2,5,1,0,4};
+        int [] sortedArray =   {0,2,1,4,5,6,9};
+        algorithm.Sort s=new algorithm.Sort();
+        int[]res=s.insertionSort(unSortedArray);
+//             for (int i =0 ;i < res.length;i++){
+//                 System.out.println(res[i]);
+//             }
+        Assert.assertArrayEquals(sortedArray,res);
+        System.out.println("test passed ");
 
     }
+    @Test
+    public void testSelction(){
+        int [] unSortedArray = {6,9,2,5,1,0,4};
+        int [] sortedArray =   {0,2,1,4,5,6,9};
+        algorithm.Sort s=new algorithm.Sort();
+        int[]res=s.selectionSort(unSortedArray);
+//             for (int i =0 ;i < res.length;i++){
+//                 System.out.println(res[i]);
+//             }
+        Assert.assertArrayEquals(sortedArray,res);
+        System.out.println("test passed ");
+
+    }
+    @Test
+    public void testSelection1(){
+        int [] unSortedArray = {6,9,2,5,1,0,4};
+        int [] sortedArray =   {0,1,2,4,5,6,9};
+        algorithm.Sort s=new algorithm.Sort();
+        int[]res=s.selectionSort(unSortedArray);
+//             for (int i =0 ;i < res.length;i++){
+//                 System.out.println(res[i]);
+//             }
+        Assert.assertArrayEquals(sortedArray,res);
+        System.out.println("test passed ");
+
+    }
+    @Test
+    public void testMerge(){
+        int [] unSortedArray = {6,9,2,5,1,0,4};
+        int [] sortedArray =   {0,1,2,4,5,6,9};
+        algorithm.Sort s=new algorithm.Sort();
+        int[]res=s.mergeSort(unSortedArray,0,unSortedArray.length-1);
+//             for (int i =0 ;i < res.length;i++){
+//                 System.out.println(res[i]);
+//             }
+        Assert.assertArrayEquals(sortedArray,res);
+        System.out.println("test passed ");
+
+    }
+    @Test
+    public void testMerge1(){
+        int [] unSortedArray = {6,9,2,5,1,0,4};
+        int [] sortedArray =   {0,6,2,4,5,6,9};
+        algorithm.Sort s=new algorithm.Sort();
+        int[]res=s.mergeSort(unSortedArray,0,unSortedArray.length-1);
+//             for (int i =0 ;i < res.length;i++){
+//                 System.out.println(res[i]);
+//             }
+        Assert.assertArrayEquals(sortedArray,res);
+        System.out.println("test passed ");
+
+    }
+    @Test
+    public void testQuickSort(){
+        int [] unSortedArray = {6,9,2,5,1,0,4};
+        int [] sortedArray =   {0,1,2,4,5,6,9};
+        algorithm.Sort s=new algorithm.Sort();
+        int[]res=s.quickSort(unSortedArray,unSortedArray.length);
+//             for (int i =0 ;i < res.length;i++){
+//                 System.out.println(res[i]);
+//             }
+        Assert.assertArrayEquals(sortedArray,res);
+        System.out.println("test passed ");
+
+    }
+    @Test
+    public void testQuickSort1(){
+        int [] unSortedArray = {6,9,2,5,1,0,4};
+        int [] sortedArray =   {0,8,2,4,5,6,9};
+        algorithm.Sort s=new algorithm.Sort();
+        int[]res=s.quickSort(unSortedArray,unSortedArray.length);
+//             for (int i =0 ;i < res.length;i++){
+//                 System.out.println(res[i]);
+//             }
+        Assert.assertArrayEquals(sortedArray,res);
+        System.out.println("test passed ");
+
+    }
+    @Test
+    public void testHeapSort(){
+        int [] unSortedArray = {6,9,2,5,1,0,4};
+        int [] sortedArray =   {0,8,2,4,5,6,9};
+        algorithm.Sort s=new algorithm.Sort();
+        int[]res=s.heapSort(unSortedArray);
+//             for (int i =0 ;i < res.length;i++){
+//                 System.out.println(res[i]);
+//             }
+        Assert.assertArrayEquals(sortedArray,res);
+        System.out.println("test passed ");
+
+    }
+    @Test
+    public void testHeapSort1(){
+        int [] unSortedArray = {6,9,2,5,1,0,4};
+        int [] sortedArray =   {0,1,2,4,5,6,9};
+        algorithm.Sort s=new algorithm.Sort();
+        int[]res=s.heapSort(unSortedArray);
+//             for (int i =0 ;i < res.length;i++){
+//                 System.out.println(res[i]);
+//             }
+        Assert.assertArrayEquals(sortedArray,res);
+        System.out.println("test passed ");
+
+    }
+    @Test
+    public void testShellSort(){
+        int [] unSortedArray = {6,9,2,5,1,0,4};
+        int [] sortedArray =   {0,1,2,4,5,6,9};
+        algorithm.Sort s=new algorithm.Sort();
+        int[]res=s.shellSort(unSortedArray);
+//             for (int i =0 ;i < res.length;i++){
+//                 System.out.println(res[i]);
+//             }
+        Assert.assertArrayEquals(sortedArray,res);
+        System.out.println("test passed ");
+
+    }
+    @Test
+    public void testShellSort1(){
+        int [] unSortedArray = {6,9,2,5,1,0,4};
+        int [] sortedArray =   {0,1,2,4,15,6,9};
+        algorithm.Sort s=new algorithm.Sort();
+        int[]res=s.shellSort(unSortedArray);
+//             for (int i =0 ;i < res.length;i++){
+//                 System.out.println(res[i]);
+//             }
+        Assert.assertArrayEquals(sortedArray,res);
+        System.out.println("test passed ");
+
+    }
+    @Test
+    public void testbucketSort(){
+        int [] unSortedArray = {6,9,2,5,1,0,4};
+        int [] sortedArray =   {0,1,2,4,15,6,9};
+        algorithm.Sort s=new algorithm.Sort();
+        int[]res=s.bucketSort(unSortedArray,10);
+//             for (int i =0 ;i < res.length;i++){
+//                 System.out.println(res[i]);
+//             }
+        Assert.assertArrayEquals(sortedArray,res);
+        System.out.println("test passed ");
+
+    }
+    @Test
+    public void testbucketSort1(){
+        int [] unSortedArray = {6,9,2,5,1,0,4};
+        int [] sortedArray =   {0,1,2,4,5,6,9};
+        algorithm.Sort s=new algorithm.Sort();
+        int[]res=s.bucketSort(unSortedArray,10);
+//             for (int i =0 ;i < res.length;i++){
+//                 System.out.println(res[i]);
+//             }
+        Assert.assertArrayEquals(sortedArray,res);
+        System.out.println("test passed ");
+
+    }
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
